d0063d4bd54d4760b7903da2d7f7339f2930e427
554am <kinnamonpayton90@gmail.com>
2024-06-23 08:06:41 -0500

    a

--- script.js
+++ script.js
@@ -3,25 +3,5 @@ function scrollToContact() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('contactForm');
-
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        Swal.fire({
-            title: "Success!",
-            text: "Your message has been sent successfully.",
-            icon: "success",
-            confirmButtonText: "OK",
-            confirmButtonColor: "#1AA34A",
-            allowOutsideClick: false,
-        }).then((result) => {
-            if (result.isConfirmed || result.dismiss === Swal.DismissReason.overlay) {
-                form.reset();
-                window.location.href = "index.html";
-            }
-        });
-    });
-
     document.getElementById('form-container').classList.add('show');
 });
